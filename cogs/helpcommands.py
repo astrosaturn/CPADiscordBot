cd572bd1bec7de04b4efc989b63eec9ada892c05
Saturn <40270386+astrosaturn@users.noreply.github.com>
2025-04-09 19:22:39 -0400

    Began work on help command

--- cogs/helpcommands.py
+++ cogs/helpcommands.py
@@ -1,13 +1,45 @@
 import disnake
+
+from disnake import Embed
 from disnake.ext import commands
+from enum import Enum
+
+def get_embed(embed_id):
+    match embed_id:
+        case 1:
+            embed = Embed(
+                title="Assignment Tracker Commands",
+                description="Information about the assignment tracker commands",
+                colour=disnake.Colour.blue()
+            )
+            embed.add_field(
+                name="/addtracker", 
+                value='''__Arguments__\n`assign_name` The Assignment's name\n`i_date` The due date (DD-MM-YYYY)\n`i_time` Due time (HH-MM)'''
+            )
+            return embed
+        case 2:
+            pass
+        case 3:
+            pass
 
 class HelpCommand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+
+    menu_options = commands.option_enum({
+        "Assignment Trackers" : 1, 
+        "Code" : 2,
+        "General" : 3
+        })
+    
+    
+    
     @commands.slash_command(description="A simple slash command")
-    async def help(self, inter: disnake.ApplicationCommandInteraction):
-        await inter.response.send_message("Hello, world!")
+    async def help(self, inter: disnake.ApplicationCommandInteraction, option: menu_options):
+        embed = get_embed(option)
+        await inter.response.send_message(embed=embed)
+        
 
 def setup(bot):
     bot.add_cog(HelpCommand(bot))
